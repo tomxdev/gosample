7daab73a59915248fa325e8e58b8ea43613b8e03
Tom <devtomx@gmail.com>
2023-05-14 22:40:10 -0300

    sample get local ip

---
+++ getIPs/get_local_ip.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func GetLocalIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddress := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddress.IP
+}
+
+func main() {
+	fmt.Println(GetLocalIP())
+}
