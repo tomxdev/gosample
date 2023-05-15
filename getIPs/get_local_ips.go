e061c72588ae67ae16daa2ac51eb369f03160dca
Tom <devtomx@gmail.com>
2023-05-14 22:40:44 -0300

    sample get local ips

---
+++ getIPs/get_local_ips.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func GetLocalIPs() ([]net.IP, error) {
+	var ips []net.IP
+	addresses, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, addr := range addresses {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ips = append(ips, ipnet.IP)
+			}
+		}
+	}
+	return ips, nil
+}
+
+func main() {
+	ips, err := GetLocalIPs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(ips)
+}
